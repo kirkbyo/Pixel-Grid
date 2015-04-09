630796ed20b9454a26f6eb1e6e4a4d5bb808686b
kirkbyo <ozzie@kirkbyo.net>
2015-04-08 20:44:08 -0400

    Add Letters (A, Z, I, E)

--- letters.js
+++ letters.js
@@ -1,13 +1,28 @@
-var fp = [3,3];
-var fpx = fp[0];
-var fpy = fp[1];
+function findLetter(letter, focalPoint) {
+   var x = letter.toLowerCase()
 
-var letterO = [fp,[fpx,fpy+1],[fpx,fpy+2],[fpx,fpy+3],[fpx,fpy+4],[fpx,fpy+5],[fpx+1,fpy+5],[fpx+2,fpy+5],[fpx+3,fpy+5],[fpx+4,fpy+5],[fpx+4,fpy],[fpx+4,fpy+1],[fpx+4,fpy+2],[fpx+4,fpy+3],[fpx+4,fpy+4], [fpx+1,fpy],[fpx+2,fpy],[fpx+3,fpy]];
+   var fp = focalPoint;
+   var fpx = fp[0];
+   var fpy = fp[1];
+   if (x == "a") {
+      return [[fpx+1,fpy],[fpx+2,fpy],[fpx+3,fpy],[fpx,fpy+1],[fpx,fpy+2],[fpx,fpy+3],[fpx,fpy+4],[fpx,fpy+5],[fpx+4,fpy+1],[fpx+4,fpy+2],[fpx+4,fpy+3],[fpx+4,fpy+4],[fpx+4,fpy+5],[fpx+1,fpy+3],[fpx+2,fpy+3],[fpx+3,fpy+3]]
+   } else if (x == "b") {
+      return []
+   } else if (x == "e") {
+      return [fp,[fpx,fpy+1],[fpx,fpy+2],[fpx,fpy+3],[fpx, fpy+4], [fpx, fpy+5],[fpx+1,fpy],[fpx+2,fpy],[fpx+3,fpy],[fpx+1,fpy+3],[fpx+2,fpy+3],[fpx+1,fpy+5],[fpx+2,fpy+5],[fpx+3,fpy+5]];
+   } else if (x == "i") {
+      return [fp, [fpx,fpy+2],[fpx,fpy+3],[fpx, fpy+4], [fpx, fpy+5]]
+   } else if (x == "o") { // Letter o
+      return [fp,[fpx,fpy+1],[fpx,fpy+2],[fpx,fpy+3],[fpx,fpy+4],[fpx,fpy+5],[fpx+1,fpy+5],[fpx+2,fpy+5],[fpx+3,fpy+5],[fpx+4,fpy+5],[fpx+4,fpy],[fpx+4,fpy+1],[fpx+4,fpy+2],[fpx+4,fpy+3],[fpx+4,fpy+4], [fpx+1,fpy],[fpx+2,fpy],[fpx+3,fpy]];
+   } else if (x == "z") { // Letter z
+      return [fp,[fpx+1,fpy],[fpx+2,fpy],[fpx+3,fpy],[fpx+3,fpy+1],[fpx+2,fpy+2],[fpx+1,fpy+3],[fpx,fpy+4],[fpx,fpy+5],[fpx+1,fpy+5],[fpx+2,fpy+5],[fpx+3,fpy+5]];
+   } else {
+      return []
+   }
+}
 
-var letterZ = []
-
-
-function drawLetter(letter) {
+function drawLetter(focalPoint, letter) {
+   var letter = findLetter(letter, focalPoint)
    for (var row=0; row<letter.length; row++) {
       var points = letter[row]
 
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 GenerateGrid()
 
-drawLetter(letterZ);
+drawLetter([3,3], "a");
+drawLetter([9,3], "z");
+drawLetter([15,3], "z");
+drawLetter([20,3], "i");
+drawLetter([22,3], "e");
